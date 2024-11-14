938f44c15a21ec26b09fbaf4ae80092819b934c2
JoshCodedit <joshua.dixoncms@gmail.com>
2024-11-14 12:10:54 +0000

    feat: implemented get colours request

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -1,18 +1,34 @@
+import { useEffect, useState } from "react";
+import { fetchData } from "../../utils/fetchData";
+
+interface Colour {
+  id: number;
+  hex: string;
+}
+
 export default function Feed() {
+  const [colours, setColours] = useState<Colour[]>([]);
+
+  const fetchColours = async () => {
+    const data: Colour[] = await fetchData("colours", "GET");
+    setColours(data);
+  };
+
+  useEffect(() => {
+    fetchColours();
+  }, []);
+
   return (
     <>
       <h2 className="text-7xl mb-32 text-center">Today's colours</h2>
-      <div className="grid grid-cols-4 w-11/12 m-auto gap-52">
-        <div className="h-52 w-52 bg-jjjYellow"></div>
-        <div className="h-52 w-52 bg-green-700"></div>
-        <div className="h-52 w-52 bg-red-800"></div>
-        <div className="h-52 w-52 bg-blue-500"></div>
-        <div className="h-52 w-52 bg-pink-500"></div>
-        <div className="h-52 w-52 bg-slate-500"></div>
-        <div className="h-52 w-52 bg-orange-400"></div>
-        <div className="h-52 w-52 bg-lime-300"></div>
-        <div className="h-52 w-52 bg-jjjYellow"></div>
-        <div className="h-52 w-52 bg-jjjYellow"></div>
+      <div className="grid grid-cols-4 w-11/12 m-auto gap-8">
+        {colours.map((colour) => (
+          <div
+            key={colour.id}
+            className="h-52 w-52"
+            style={{ backgroundColor: `#${colour.hex}` }}
+          ></div>
+        ))}
       </div>
     </>
   );
