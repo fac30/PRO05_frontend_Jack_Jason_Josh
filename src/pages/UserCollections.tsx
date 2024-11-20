e84eec03abbdaeb2fe306174b1ae2369ebe478a0
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-11-20 23:24:28 +0000

    style: collections look nice and only show 3 colors

--- src/pages/UserCollections.tsx
+++ src/pages/UserCollections.tsx
@@ -2,6 +2,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import { useState, useEffect, useCallback } from "react";
 import { Button } from "@mui/material";
 import CollectionModal from "../components/CollectionModal/CollectionModal";
+import ColourGrid from "../components/ColourGrid/ColourGrid";
 
 interface Collection {
   id: string;
@@ -54,16 +55,34 @@ export default function UserCollections() {
 
   return (
     <div>
-      <h1>My Collections</h1>
-      {collections.map((collection) => (
-        <h2
-          key={collection.id}
-          onClick={() => handleCollectionClick(collection)}
-          style={{ cursor: "pointer", color: "blue" }}
-        >
-          {collection.name}
-        </h2>
-      ))}
+      <h1 className="text-5xl text-center mb-10">My Collections</h1>
+      <div className="flex flex-wrap grid-cols-2">
+        {collections.map((collection) => (
+          <>
+            <div className="border mb-10  m-auto bg-gray-100 shadow-sm pb-8">
+              <div className=" bg-gray-500 py-4 mb-8">
+                <h2
+                  className=" ml-5 text-3xl text-white"
+                  key={collection.id}
+                  onClick={() => handleCollectionClick(collection)}
+                  style={{ cursor: "pointer" }}
+                >
+                  {collection.name}
+                </h2>
+              </div>
+              {collection &&
+                collection.colourCollections &&
+                Array.isArray(collection.colourCollections) && (
+                  <ColourGrid
+                    coloursArray={collection.colourCollections
+                      .map((item) => item.colour) // Get the colour object from each item
+                      .slice(0, 2)} // Take the first 3 colours
+                  />
+                )}
+            </div>
+          </>
+        ))}
+      </div>
       <Button onClick={handleOpen} size="large" color="success">
         Create collection
       </Button>
