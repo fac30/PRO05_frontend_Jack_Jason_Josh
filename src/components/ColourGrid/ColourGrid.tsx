4d0cccff84b405c72c8db9dabf6aa67ebc598901
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-11-20 17:16:02 +0000

    feat: wip modal

--- src/components/ColourGrid/ColourGrid.tsx
+++ src/components/ColourGrid/ColourGrid.tsx
@@ -4,6 +4,9 @@ import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { Box } from "@mui/material";
+import PaletteModal from "./PaletteModal.tsx/PaletteModal";
+import { useState, useEffect } from "react";
+import { useAuth } from "../../contexts/AuthContext";
 
 interface Colour {
   id: number;
@@ -16,6 +19,41 @@ interface ColourGridProps {
 }
 
 export default function ColourGrid({ coloursArray }: ColourGridProps) {
+  const [collections, setCollections] = useState<[]>([]); // Ensure it's an array by default
+  const { isAuthenticated, username, loading, logout, userId } = useAuth();
+  // console.log(userId);
+
+  const getCollections = async () => {
+    const data = await fetch(
+      `http://localhost:5187/collections/user/${userId}`,
+      {
+        method: "GET", // or whatever method you need (POST, PUT, etc.)
+        headers: {
+          "Content-Type": "application/json",
+          // You can add other headers here if needed
+        },
+        credentials: "include", // This will send cookies and authentication data
+      }
+    );
+
+    const myData = await data.json();
+
+    // console.log(myData);
+
+    setCollections(myData);
+  };
+
+  useEffect(() => {
+    getCollections();
+  }, [userId]);
+
+  const [open, setOpen] = useState(false);
+
+  // Function to handle opening the modal
+  const handleOpen = () => setOpen(true);
+  // Function to handle closing the modal (this would be passed to PaletteModal)
+  const handleClose = () => setOpen(false);
+
   return (
     <div className="grid grid-cols-4 w-11/12 m-auto gap-8">
       {coloursArray.map((colour) => (
@@ -40,7 +78,14 @@ export default function ColourGrid({ coloursArray }: ColourGridProps) {
           <CardActions>
             <Box display={"flex"} justifyContent={"center"} width={"100%"}>
               <Button size="medium">Favourite</Button>
-              <Button size="medium">Add to Palette</Button>
+              <Button onClick={handleOpen} size="medium">
+                Add to Palette
+              </Button>
+              <PaletteModal
+                open={open}
+                setOpen={setOpen}
+                userCollections={collections}
+              />
             </Box>
           </CardActions>
         </Card>
